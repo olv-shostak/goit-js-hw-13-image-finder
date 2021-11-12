4807e8211fb665fed24970732add180b580c9652
Oleksandr Shostak <olv.shostak@gmail.com>
2021-11-12 20:42:21 +0300

    finished task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,103 @@
+
 import './sass/main.scss';
+import refs from './js/refs';
+import cardMarkup from './templates/card-markup.hbs';
+import * as basicLightbox from 'basiclightbox';
+import { info, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import './sass/basicLightbox.min.css';
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+refs.list.addEventListener('click', onPictureClick);
+
+let page = 1;
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const value = e.currentTarget.elements.query.value;
+  if (value === '') {
+    return info({
+      text: 'Enter the value!',
+      delay: 2000,
+      closerHover: true,
+    });
+  }
+
+  const BASE_URL = 'https://pixabay.com/api/';
+  const queryParam = new URLSearchParams({
+    key: '24305587-c8482d095dc3290807d6dab36',
+    image_type: 'photo',
+    q: refs.form.elements.query.value,
+    orientation: 'horizontal',
+    page: 1,
+    per_page: 12,
+  });
+
+  fetch(`${BASE_URL}?${queryParam}&page=${page}`)
+    .then(res => res.json())
+    .then(data => {
+      refs.loadMoreBtn.classList.remove('is-hidden');
+      renderCard(data);
+    });
+
+  function renderCard({ hits }) {
+    if (hits.length === 0) {
+      refs.loadMoreBtn.classList.add('is-hidden');
+      error({
+        text: 'No matches found!',
+        delay: 2000,
+        closerHover: true,
+      });
+    }
+    refs.list.innerHTML = cardMarkup(hits);
+  }
+}
+
+function incrementPage() {
+  page += 1;
+}
+
+function onLoadMoreBtnClick() {
+  incrementPage();
+
+  const BASE_URL = 'https://pixabay.com/api/';
+  const queryParam = new URLSearchParams({
+    key: '24305587-c8482d095dc3290807d6dab36',
+    image_type: 'photo',
+    q: refs.form.elements.query.value,
+    orientation: 'horizontal',
+    page: 1,
+    per_page: 12,
+  });
+
+  fetch(`${BASE_URL}?${queryParam}&page=${page}`)
+    .then(res => res.json())
+    .then(data => {
+      renderCard(data);
+      refs.loadMoreBtn.classList.remove('is-hidden');
+      buttonClick();
+    });
+
+  function renderCard({ hits }) {
+    const markup = cardMarkup(hits);
+    refs.list.insertAdjacentHTML('beforeend', markup);
+  }
+}
+const hiddenElement = refs.loadMoreBtn;
+
+function buttonClick() {
+  hiddenElement.scrollIntoView({ block: 'center', behavior: 'smooth' });
+}
+
+function onPictureClick(e) {
+  if (!e.target.classList.contains('card-img')) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.largeImg}" width="800" height="600">
+  `);
+  instance.show();
+}
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export default {
+    form: document.querySelector('.search-form'),
+    formBtn: document.querySelector('.js-form-btn'),
+    loadMoreBtn: document.querySelector('.js-load-btn'),
+    list: document.querySelector('.gallery'),
+    goTopBtn: document.querySelector('.back_to_top'),
+  };
